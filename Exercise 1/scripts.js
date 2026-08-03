f49da4cca05922e5b9e871b28f2df06903b965e3
Hussein Hassan Abdi <125039384+husseinhassanabdi@users.noreply.github.com>
2026-08-03 21:13:31 +0300

    i submited my exercise 1

---
+++ Exercise 1/scripts.js
@@ -0,0 +1,17 @@
+
+let x = 7;
+let y = 3;
+
+console.log("Addition:", x + y);
+
+
+console.log("Subtraction:", x - y);
+
+
+console.log("Multiplication:", x * y);
+
+console.log("Division:", x / y);
+
+console.log("Modulus:", x % y);
+
+console.log("Exponentiation:", x ** y);
