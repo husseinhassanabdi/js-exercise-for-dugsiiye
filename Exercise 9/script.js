5d4065dba88e13f296a1617a2b674b82d29a24c5
Hussein Hassan Abdi <125039384+husseinhassanabdi@users.noreply.github.com>
2026-08-06 15:18:04 +0300

    submited exercise 9

---
+++ Exercise 9/script.js
@@ -0,0 +1,16 @@
+
+
+let car = {
+    make : "Toyota",
+    model : "Camry",
+    year : 2020,
+    start : function() {
+        console.log("The Car has started"); 
+    }
+};
+
+car.start();
+
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
