95d320fc8485fe2474c373b95c647c44e6deab9e
Hussein Hassan Abdi <125039384+husseinhassanabdi@users.noreply.github.com>
2026-08-09 09:35:06 +0300

    submited exercise 15

---
+++ Exercise 15/script.js
@@ -0,0 +1,22 @@
+
+
+const people = [
+    { name: "John", age: 30, birthday: "1993-05-15" },
+    { name: "Jane", age: 25, birthday: "1998-08-20" },
+    { name: "Bob", age: 35, birthday: "1988-12-10" }
+]
+
+// for (const key of people) {
+//     console.log(key)
+// }
+
+console.log("Properties and values of the object:");
+for (let student of people) {  {
+    console.log(student.name);
+    console.log(student.age);
+    console.log(student.birthday);
+    
+    
+}
+ console.log(" -------" )
+}
