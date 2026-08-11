a4667bfaadca76c889c4c2881e68f6a78fff6a7a
Hussein Hassan Abdi <125039384+husseinhassanabdi@users.noreply.github.com>
2026-08-11 07:41:48 +0300

    submited exercise 25

---
+++ Exercise 25/script.js
@@ -0,0 +1,16 @@
+
+//spread operator
+const numbers = [1,2,3];
+
+const allNumbers = [...numbers, 4, 5, 6];
+console.log(allNumbers);
+
+
+//rest operator
+
+function multiply(...numbers) {
+    return numbers.reduce((total, num) => total * num, 1);
+}
+
+console.log(multiply(10, 20, 30));
+
